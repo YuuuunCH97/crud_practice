25676e5e718ecfac0293cbd75c0ddf809ae89db8
changchingyun <changchingyun@CHINGYUN>
2024-06-08 00:03:35 +0800

    index

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
-var express = require("express");
+const express = require("express");
 const bcrypt = require("bcryptjs");
-var router = express.Router();
+const router = express.Router();
 const fs = require('fs');
 
 const bodyParser = require("body-parser"); 
@@ -28,64 +28,44 @@ router.post("/add", function (req, res, next) {
   res.send(`number: ${number}`);
 });
 
-router.get("/ejs_example", function (req, res, next) {
-  res.render("ejs_example", { user: "xiaoswaii", show: true });
-});
-
-
-router.get("/ejs_example1", function (req, res, next) {
-  res.render("ejs_example1", {
-    user: "xiaoswaii",
-    show: false,
-    title: "<h3>Welcome</h3>",
-    animal: ["dog", "cat", "tiger"],
-  });
-});
-
-router.get("/registera", function (req, res, next) {
-  res.render("register", { user: "xiaoswaii", show: true });
-});
 
 
 // 中介軟體來解析 JSON 和 URL 編碼的表單數據
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
-
+// 渲染創建會員頁面
 router.get("/create_member", function (req, res, next) {
+  console.log("Rendering create_member page");
   res.render("create_member");
 });
 
-
+// 渲染購物車系統頁面
 router.get("/shop_sys", function (req, res, next) {
   res.render("shop_sys");
 });
 
-
+// 查詢會員
 router.get("/search_member", function (req, res, next) {
   
   // 寫入到json >> public/data/data.json
   const newData = { user: 'xxx@gmail.com' };
-  fs.writeFile('public/data/data.json', JSON.stringify(newData, null, 2), (err) => {
+
+  fs.writeFile(path.join(__dirname, '../public/data/data.json'), JSON.stringify(newData, null, 2), (err) => {
     if (err) {
       console.error('Error writing file:', err);
       return;
     }
-  })
+  });
 
-  let jsonData
-  // 讀取json >> public/data/data.json
-  fs.readFile('public/data/data.json', 'utf8', (err, data) => {
+  fs.readFile(path.join(__dirname, '../public/data/data.json'), 'utf8', (err, data) => {
     if (err) {
       console.error('Error reading file:', err);
       return;
     }
-    jsonData = JSON.parse(data);
-
-    // 將資料傳給前端
+    const jsonData = JSON.parse(data);
     res.render("search_member", jsonData);
   });
-
 });
 
 
@@ -101,7 +81,7 @@ router.get("/create_member", function (req, res, next) {
 router.get('/edit_member/:email', function (req, res, next) {
   const email = req.params.email;
 
-  fs.readFile('public/data/data.json', 'utf8', (err, data) => {
+  fs.readFile(path.join(__dirname, '../public/data/data.json'), 'utf8', (err, data) => {
     if (err) {
       console.error('Error reading file:', err);
       return res.status(500).send('Internal Server Error');
@@ -133,12 +113,12 @@ router.get('/edit_member/:email', function (req, res, next) {
 router.post("/create_member", function (req, res, next) {
   const memberData = req.body;
 
-  fs.readFile('public/data/data.json', 'utf8', (err, data) => {
+  fs.readFile(path.join(__dirname, '../public/data/data.json'), 'utf8', (err, data) => {
     if (err) {
       console.error('讀取文件時發生錯誤:', err);
       return res.status(500).send("內部伺服器錯誤");
     }
-    
+
     let jsonData;
     try {
       jsonData = JSON.parse(data);
@@ -147,10 +127,13 @@ router.post("/create_member", function (req, res, next) {
       return res.status(500).send("內部伺服器錯誤");
     }
 
+    // 確保 members 數組存在，如果不存在則初始化為空數組
     jsonData.members = jsonData.members || [];
+    // 將新的會員數據添加到 members 數組中
     jsonData.members.push(memberData);
 
-    fs.writeFile('public/data/data.json', JSON.stringify(jsonData, null, 2), (err) => {
+    // 將更新後的數據寫回 'public/data/data.json' 文件
+    fs.writeFile(path.join(__dirname, '../public/data/data.json'), JSON.stringify(jsonData, null, 2), (err) => {
       if (err) {
         console.error('寫入文件時發生錯誤:', err);
         return res.status(500).send("內部伺服器錯誤");
@@ -168,7 +151,7 @@ router.post("/edit_member/:email", function (req, res, next) {
   const email = req.params.email;
   const updatedMemberData = req.body;
 
-  fs.readFile('public/data/data.json', 'utf8', (err, data) => {
+  fs.readFile(path.join(__dirname, '../public/data/data.json'), 'utf8', (err, data) => {
     if (err) {
       console.error('Error reading file:', err);
       return res.status(500).send("Internal Server Error");
@@ -192,7 +175,7 @@ router.post("/edit_member/:email", function (req, res, next) {
     // Update member data
     jsonData.members[memberIndex] = updatedMemberData;
 
-    fs.writeFile('public/data/data.json', JSON.stringify(jsonData, null, 2), (err) => {
+    fs.writeFile(path.join(__dirname, '../public/data/data.json'), JSON.stringify(jsonData, null, 2), (err) => {
       if (err) {
         console.error('Error writing file:', err);
         return res.status(500).send("Internal Server Error");
@@ -213,21 +196,11 @@ router.post("/edit_member/:email", function (req, res, next) {
         member: updatedMemberData,
         showAlert: true
       });
-
-
     });
   });
 });
 
-
-
-
-
-
-//shop_search
-
-//渲染 shop_search 页面
-
+// 渲染 shop_search 页面
 router.get("/shop_search", function (req, res) {
   res.render("shop_search", { data: [] });
 });
@@ -237,7 +210,7 @@ router.post("/shop_search", function (req, res) {
   const startDate = req.body['start-date'] ? new Date(req.body['start-date']) : null;
   const endDate = req.body['end-date'] ? new Date(req.body['end-date']) : null;
   const email = req.body.email;
-  // 检查日期或邮箱是否未填写
+
   if (!startDate || !endDate || !email) {
     // 如果其中一个没有填写，返回错误消息并渲染空的数据
     return res.render("shop_search", { data: [], errorMessage: "請填寫日期或帳號" });
@@ -246,37 +219,32 @@ router.post("/shop_search", function (req, res) {
 
   // 读取 shop_searchdata.json 文件
   fs.readFile(path.join(__dirname, '../public/data/shop_searchdata.json'), 'utf8', (err, data) => {
-      if (err) {
-          console.error('读取文件错误:', err);
-          return res.status(500).send("Internal Server Error");
-      }
+    if (err) {
+      console.error('读取文件错误:', err);
+      return res.status(500).send("Internal Server Error");
+    }
 
-      let jsonData;
-      try {
-          jsonData = JSON.parse(data);
-      } catch (err) {
-          console.error('解析 JSON 错误:', err);
-          return res.status(500).send("Internal Server Error");
-      }
+    let jsonData;
+    try {
+      jsonData = JSON.parse(data);
+    } catch (err) {
+      console.error('解析 JSON 错误:', err);
+      return res.status(500).send("Internal Server Error");
+    }
 
       // 过滤符合条件的数据
-      const filteredData = jsonData.filter(item => {
-          const purchaseDate = new Date(item.purchaseDate);
-          if ((startDate && purchaseDate < new Date(startDate)) || 
-              (endDate && purchaseDate > new Date(endDate))) {
-              return false;
-          }
-          if (email && item.email !== email) {
-              return false;
-          }
-          return true;
-      });
+    const filteredData = jsonData.filter(item => {
+      const purchaseDate = new Date(item.purchaseDate);
+      if ((startDate && purchaseDate < startDate) || (endDate && purchaseDate > endDate)) {
+        return false;
+      }
+      if (email && item.email !== email) {
+        return false;
+      }
+      return true;
+    });
 
-      // 渲染包含过滤后数据的表格
-      console.log('Filtered Data:', filteredData);
-      console.log(data); // 在这里输出 data 变量的值
-      res.render("shop_search", { data: filteredData , errorMessage: null});
-
+    res.render("shop_search", { data: filteredData, errorMessage: null });
   });
 });
 
