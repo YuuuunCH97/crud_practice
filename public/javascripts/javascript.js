860217f3ade00bf29eaac611cd8f973cad364418
bard <xx61509xx@gmail.com>
2024-07-21 23:30:26 +0800

    🔁 refact: updateCountryCities and use axios

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,34 +1,46 @@
 function updateCountry() {
-    // 透過API取得國家資料
-    $.ajax({
-      type: 'POST',
-      url: '/get_country_city',
-      data: {},
-      success: function (msg) {
-        let country_html = '';
-        for (var key in msg['receivedData']) {
-          country_html += `<option value='${msg['receivedData'][key]}'>${msg['receivedData'][key]}</option>`;
-        }
-        $('#select_country').html(country_html);
-        updateCities();
-      },
-    });
+  return axios
+  .post('/get_country_city')
+  .then(function (response) {
+    let country_html = '';
+    for (var key in response.data['receivedData']) {
+      country_html += `<option value='${response.data['receivedData'][key]}'>${response.data['receivedData'][key]}</option>`;
+    }
+    $('#select_country').html(country_html);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {
+  });
   }
-
 
   // 國家相對應城市
   function updateCities() {
     var country = document.getElementById('select_country').value;
-    $.ajax({
-      type: 'POST',
-      url: '/get_country_city',
-      data: { country: country },
-      success: function (msg) {
-        let city_html = '';
-        for (var key in msg['receivedData']) {
-          city_html += `<option value='${msg['receivedData'][key]}'>${msg['receivedData'][key]}</option>`;
-        }
-        $('#select_city').html(city_html);
-      },
+    return axios
+    .post('/get_country_city', {country: country})
+    .then(function (response) {
+      let city_html = '';
+      for (var key in response.data['receivedData']) {
+        city_html += `<option value='${response.data['receivedData'][key]}'>${response.data['receivedData'][key]}</option>`;
+      }
+      $('#select_city').html(city_html);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .finally(function () {
     });
+  }
+
+  async function updateCountryCities(Country=undefined, city=undefined) {
+    await updateCountry();
+    if (Country){
+      $('#select_country').val(Country);
+    }
+    await updateCities();
+    if (city){
+      $('#select_city').val(city);
+    }
   }
